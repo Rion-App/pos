28fcfeefc1051d068ecd83765429dcf1a2f54c45
ronypermadi <ronnypermadi1@gmail.com>
2024-08-31 23:01:42 +0700

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+    server: {
+        https: true,
+        host: process.env.APP_URL,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -17,4 +24,15 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        manifest: true, // This ensures the manifest.json is generated
+        outDir: 'public/build', // Output directory
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]',
+            },
+        },
+    },
 });
